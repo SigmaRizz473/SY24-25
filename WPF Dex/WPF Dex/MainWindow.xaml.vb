ada2fabc123474f9e171c3cfd279f936ca709b9b
SigmaRizz473 <eahin887@learner.dvusd.org>
2024-12-03 13:38:28 -0700

    created new dex

---
+++ WPF Dex/WPF Dex/MainWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Dragapult.Visibility = Visibility.Hidden
+        ShinyDragapult.Visibility = Visibility.Visible
+        dragapultlabel.Content = "placeholder text"
+
+
+    End Sub
+End Class
